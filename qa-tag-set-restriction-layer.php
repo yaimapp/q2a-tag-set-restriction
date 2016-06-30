7016aa16633de26c62ad854668e237a23f9b269d
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-06-30 10:57:50 +0900

    Add layer file

---
+++ qa-tag-set-restriction-layer.php
@@ -0,0 +1,17 @@
+<?php
+
+class qa_html_theme_layer extends qa_html_theme_base
+{
+
+	function main()
+	{
+		if ($this->template == 'ask') {
+			if (!qa_is_logged_in() || qa_get_logged_in_level() < QA_USER_LEVEL_SUPER) {
+				$this->content['form']['fields']['tags']['label'] = '';
+				$this->content['form']['fields']['tags']['tags'] .= ' style="display: none;"';
+			}
+		}
+		qa_html_theme_base::main();
+	}
+
+}
