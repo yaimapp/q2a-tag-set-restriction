4b03b524d86683eaac25c0017514d23901b41c3f
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-06-30 11:13:13 +0900

    Add option for tag form hide conditon

--- qa-tag-set-restriction-layer.php
+++ qa-tag-set-restriction-layer.php
@@ -6,7 +6,9 @@ class qa_html_theme_layer extends qa_html_theme_base
 	function main()
 	{
 		if ($this->template == 'ask') {
-			if (!qa_is_logged_in() || qa_get_logged_in_level() < QA_USER_LEVEL_SUPER) {
+			if (!qa_is_logged_in() ||
+				(qa_opt('q2a_tag_set_restriction_enabled') &&
+				 qa_get_logged_in_level() < QA_USER_LEVEL_SUPER)) {
 				$this->content['form']['fields']['tags']['label'] = '';
 				$this->content['form']['fields']['tags']['tags'] .= ' style="display: none;"';
 			}
