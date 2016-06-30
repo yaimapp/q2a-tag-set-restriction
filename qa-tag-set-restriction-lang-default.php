4db6b29e8515788a1de70ecfca831b0153308ef2
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-06-30 11:12:17 +0900

    Add translation files

---
+++ qa-tag-set-restriction-lang-default.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+	Plugin Name: Tag Set Restriction
+	Plugin URI:
+	Plugin Description: A tag bill is restricted to an administrator.
+	Plugin Version: 1.0
+	Plugin Date: 2016-06-30
+	Plugin Author:38qa.net
+	Plugin Author URI:
+	Plugin License: GPLv2
+	Plugin Minimum Question2Answer Version: 1.7
+	Plugin Update Check URI:
+*/
+
+	return array(
+		// default
+		'enable_plugin' => 'Enable the Plugin',
+	);
+
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
---
+++ qa-tag-set-restriction-lang-ja.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+	Plugin Name: Tag Set Restriction
+	Plugin URI:
+	Plugin Description: A tag bill is restricted to an administrator.
+	Plugin Version: 1.0
+	Plugin Date: 2016-06-30
+	Plugin Author:38qa.net
+	Plugin Author URI:
+	Plugin License: GPLv2
+	Plugin Minimum Question2Answer Version: 1.7
+	Plugin Update Check URI:
+*/
+
+	return array(
+		// japanese
+		'enable_plugin' => 'プラグインを有効にする',
+	);
+
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
